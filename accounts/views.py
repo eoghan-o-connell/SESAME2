21f4f465ac6ae151707cb424783b2eb3a630ba67
Aidan Miskella <aidanmiskella@Aidans-MacBook-Air.local>
2019-01-27 14:49:20 +0000

    Links between each page is done for logged in users and logged out users

--- accounts/views.py
+++ accounts/views.py
@@ -6,14 +6,12 @@ from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
-    numbers = [1,2,3,4,5]
-    name = 'Aidan Miskella'
-
-    args = {'myName': name, 'numbers': numbers}
-    return render(request, 'accounts/home.html', args)
+    return render(request, 'accounts/home.html')
 
 def register(request):
     if request.method =='POST':
@@ -27,10 +25,12 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
+@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
+@login_required
 def edit_profile(request):
     if request.method=='POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -43,6 +43,7 @@ def edit_profile(request):
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
 
+@login_required
 def change_password(request):
     if request.method=="POST":
         form = PasswordChangeForm(data=request.POST, user=request.user)
