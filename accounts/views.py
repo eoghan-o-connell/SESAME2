df4d46b94d539226b25a03e91459d236fb8b82be
Aidan Miskella <aidanmiskella@Aidans-MacBook-Air.local>
2019-01-27 16:47:34 +0000

    Url links dynamic Custom middleware require login to access pages

--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
-@login_required
 def home(request):
     return render(request, 'accounts/home.html')
 
@@ -18,19 +18,17 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid:
             form.save()
-            return redirect('/account')
+            return redirect(reverse('home'))
     else:
         form = RegistrationForm()
 
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
-@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
-@login_required
 def edit_profile(request):
     if request.method=='POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -43,7 +41,6 @@ def edit_profile(request):
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
 
-@login_required
 def change_password(request):
     if request.method=="POST":
         form = PasswordChangeForm(data=request.POST, user=request.user)
@@ -51,10 +48,10 @@ def change_password(request):
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
-            return redirect('/account/profile')
+            return redirect(reverse('view_profile'))
 
         else:
-            return redirect('/account/change-password')
+            return redirect(reverse('change_password'))
 
     else:
         form = PasswordChangeForm(user=request.user)
--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'project.middleware.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -121,6 +122,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/account/'
+
+LOGIN_URL = '/account/login/'
+
+LOGIN_EXEMPT_URLS = (
+    r'^account/logout/$',
+    r'^account/register/$',
+
+)
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
