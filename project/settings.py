ce5bfa778b1d19626c961f7746b2bb0629cd9031
Author Name <email@address.com>
2019-02-06 13:47:22 +0000

    Latest commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i+&e!gr-p+$y!5gxn&gidcbj@-%bn8)ckk5a5g-q4z0o!bq=ac'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["nadehh.netsoc.co"]
+ALLOWED_HOSTS = ["nadehh.netsoc.co", "127.0.0.1"]
 
 
 # Application definition
