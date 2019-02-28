7a1b9439fa3931039872cd77ffc26133610263d5
Ann <ann@project.git-is-it.org>
2019-02-28 18:33:20 +0000

    Removed comments from js

--- home/static/publishCalls/newdragNdropCalls.js
+++ home/static/publishCalls/newdragNdropCalls.js
@@ -3,80 +3,18 @@ window.onload = setup();
 var dz;
 var progressbar;
 
-//REMOVE BUTTON AND ACCEPT GET REQUEST .. spawn a link tag and re direct to same page
-
-
-var fname;
-var sname;
-var date;
-var tag;
 var csrftoken;
 var uploadButton;
-var form;
-var title;
-var description;
-var grant;
 var call_id;
 var editing_mode;
-var captureFiles;
-var toggleButton;
-var summary;
-var summaryHeading;
-
-var dict = {
-  "fname" : 0,
-  "sname" : 1,
-  "title" : 2,
-  "grant" : 3
-};
-
-var elements;
-
-var previousFname="";
-var previousLname="";
-var previous;
-var element;
-// var fnamePrevBool;
 
 function setup(){
     Dropzone.autoDiscover = false;
-
-    // toggleButton = document.getElementById('collapsingButton');
-    // fname = document.getElementById('fname');
-    // sname = document.getElementById('sname');
-    // date = document.getElementById('date');
-    // tag = document.getElementById('tag');
-    // title = document.getElementById('title');
-    // description = document.getElementById('description');
-    // grant = document.getElementById('grant');
-     call_id = document.getElementById('call_id');
-    // editing_mode = document.getElementById('updating_call');
-    // summary = document.getElementById("summary");
-    // summaryHeading=document.getElementById('summaryHeading');
-    //
+    call_id = document.getElementById('call_id');
     progressbar = document.getElementById('progress');
     uploadButton = document.getElementById("btn");
     csrftoken = Cookies.get('csrftoken');
-    //
-    // fname.addEventListener("input",checkValidName,false);
-    // sname.addEventListener("input",checkValidName,false);
-    // title.addEventListener("input",checkValidName,false);
-    // grant.addEventListener("input",checkValidName,false);
-    //
-    // toggleButton.addEventListener("click",checkSummary,false);
     uploadButton.addEventListener("click",uploadFiles,false);
-
-    // toggleButton.click();
-
-    // elements = [fname,sname,title,grant];
-
-    // for (var i=0; i<elements.length; i++){
-    //     if (elements[i].value!=""){
-    //         let element = elements[i];
-    //         element.classList.remove('is-invalid');
-    //         element.classList.add('is-valid');
-    //     }
-    // }
 
     form = document.getElementById('checkForm');
 
@@ -95,13 +33,11 @@ function setup(){
         url: "/home/call_view",
         init: function() {
             this.on("totaluploadprogress",uploadInformation);
-            this.on("addedfile", fileAdded);
             this.on("successmultiple",success);
             this.on("sendingmultiple",sndMul);
             this.on("maxfilesexceeded", function(file) { this.removeFile(file); });
         }
     });
-//        dz.createThumbnailFromUrl("pdf_thumbnail.webp","../pdf_thumbnail.webp");
   }
 }
 
@@ -127,15 +63,6 @@ function checkValidName(e){
 
  function sndMul(file,xhr,formData){
        formData.append("call_id",call_id.value);
-//     formData.append("sname",sname.value);
-//     formData.append("date",date.value);
-//     formData.append("tag",tag.value);
-//     formData.append("title",title.value);
-//     formData.append("description",description.value);
-//     formData.append("grant",grant.value);
-//     formData.append("editing_mode",editing_mode.value);
-//     formData.append("_call_id",call_id.value);
-//     console.log("**********" + tag.value);
 }
 
 function success(ev){
@@ -144,27 +71,6 @@ function success(ev){
     console.log("removed all files");
     progressbar.style.width = 0 + "%";
     progressbar.innerHTML = 0 + "%";
-    // var s_title = document.getElementById('s_title');
-    // var s_desc =  document.getElementById('s_desc');
-    // var s_amount = document.getElementById('s_amount');
-    // var s_deadline  = document.getElementById('s_deadline');
-    // var s_call  = document.getElementById('s_call_pdf');
-    //
-    // s_title.innerHTML = title.value;
-    // s_desc.innerHTML = description.value;
-    // s_amount.innerHTML = grant.value;
-    // s_deadline.innerHTML = date.value;
-    // let string = "";
-    // console.log(captureFiles);
-    // for (var i=0; i<captureFiles.length; i++){
-    //    string += captureFiles[i].name + " ";
-    // }
-    // s_call.innerHTML = string;
-    // summaryHeading.innerHTML="Thank you for submitting "+ fname.value  +"!";
-
-    // form.reset();
-    // toggleButton.click();
-    // summary.style.visibility="visible";
 }
 
 function uploadInformation(progress,totalBytes,bytesSent){//, progress, bytesSent){
@@ -174,26 +80,10 @@ function uploadInformation(progress,totalBytes,bytesSent){//, progress, bytesSen
     }
 }
 
-function fileAdded(file){
-  // file.previewElement.querySelector("img").src = newname;
-
-    // You could of course generate another image yourself here,
-    // and set it as a data url.
- }
 
 function uploadFiles(ev){
-    // if (form.checkValidity()){
       if (dz.getQueuedFiles().length > 0){
         captureFiles = dz.getQueuedFiles();
-        summaryGeneration();
         dz.processQueue();
       }
-    // }
-    // form.reportValidity();
-}
-
-
-
-function summaryGeneration(){
-
 }
