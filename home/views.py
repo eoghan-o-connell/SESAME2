2dd4ac3b2dbfa496eedba9eafb8b95248f702156
Ann <ann@project.git-is-it.org>
2019-03-02 21:30:25 +0000

    minor bug fixes

--- home/views.py
+++ home/views.py
@@ -79,7 +79,7 @@ def get_call_view(request):
             with open("%s/%s"%(userFileDir,file.name),"wb+") as saveFile:
                 for line in file:
                     saveFile.write(line)
-                
+
         print(filenames)
         filename = ','.join(filenames)
 
