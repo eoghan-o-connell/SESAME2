58f7b898450cfc9b31369ca46895a290a16ed80a
= <matt@matt.matt>
2019-03-03 13:52:45 +0000

    fixed imports that were removed

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
 from django.shortcuts import render, HttpResponse, redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 from django.urls import reverse
 from django.views.static import serve
 from django.http import JsonResponse
@@ -31,7 +33,7 @@ class HomeView(TemplateView):
         return render(request, self.template_name, context)
 
 def view_center(request):
-    center_obj = Center.objects.filter(admin_id=request.user.id).values()
+    center_obj = Center.objects.filter(members=request.user.id)
     context = {'center_obj':center_obj}
     return render(request, 'home/view_center.html', context)
 
